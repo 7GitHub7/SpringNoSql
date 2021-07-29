ff866b46b71d778dd83da5126e9b46d5a7cc8c2b
Piotrek <amrozp@gmail.com>
2021-07-29 19:02:40 +0200

    Added repository

--- src/main/java/com/example/mongoTest/AccessingDataMongodbApplication.java
+++ src/main/java/com/example/mongoTest/AccessingDataMongodbApplication.java
@@ -4,19 +4,39 @@ package com.example.mongoTest;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.List;
 
 @SpringBootApplication
-public class AccessingDataMongodbApplication implements CommandLineRunner {
+public class AccessingDataMongodbApplication {
 
 
     public static void main(String[] args) {
         SpringApplication.run(AccessingDataMongodbApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
+    @Bean
+    CommandLineRunner runner(StudentRepository repository) {
+        return args -> {
+            Address address = new Address("POL","WRO","80-700");
+            Student student = new Student(
+                    "Ludwik",
+                    "Krychowiak",
+                    "krycha@onet.pl",
+                    Gender.MALE,
+                    address,
+                    List.of("CS"),
+                    BigDecimal.TEN,
+                    LocalDateTime.now()
 
-
+            );
+            repository.insert(student);
+        };
     }
 
 }
+
--- src/main/java/com/example/mongoTest/Address.java
+++ src/main/java/com/example/mongoTest/Address.java
@@ -1,11 +1,12 @@
 package com.example.mongoTest;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Address {
     private String country;
     private String city;
     private String postCode;
-
 }
