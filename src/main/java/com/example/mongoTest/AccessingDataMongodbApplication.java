ee218512651911185f3fd3edb90c67f6bfdcecdf
Piotrek <amrozp@gmail.com>
2021-07-29 23:07:57 +0200

    Added Service, Controller and API layers

--- src/main/java/com/example/mongoTest/AccessingDataMongodbApplication.java
+++ src/main/java/com/example/mongoTest/AccessingDataMongodbApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.time.ZonedDateTime;
 import java.util.List;
 
 @SpringBootApplication
@@ -20,23 +22,47 @@ public class AccessingDataMongodbApplication {
     }
 
     @Bean
-    CommandLineRunner runner(StudentRepository repository) {
+    CommandLineRunner runner(StudentRepository repository, MongoTemplate mongoTemplate) {
         return args -> {
-            Address address = new Address("POL","WRO","80-700");
+            Address address = new Address("POL", "WRO", "80-700");
+            String email = "krycha@onet.pl";
             Student student = new Student(
                     "Ludwik",
                     "Krychowiak",
-                    "krycha@onet.pl",
+                    email,
                     Gender.MALE,
                     address,
-                    List.of("CS"),
+                    List.of("CS", "Math"),
                     BigDecimal.TEN,
                     LocalDateTime.now()
 
             );
-            repository.insert(student);
+//            usingMongoTemplateAndQuery(repository, mongoTemplate, email, student);
+            repository.findStudentByEmail(email).ifPresentOrElse(s -> {
+                System.out.println(s +  " already exist");
+            }, () -> {
+                System.out.println("Inserting student ");
+                repository.insert(student);
+            });
         };
     }
+
+//    private void usingMongoTemplateAndQuery(StudentRepository repository, MongoTemplate mongoTemplate, String email, Student student) {
+//        Query query = new Query();
+//        query.addCriteria(Criteria.where("email").is(email));
+//        List<Student> students = mongoTemplate.find(query, Student.class);
+//
+//        if (students.size() > 1) {
+//            throw new IllegalStateException("found many students with email " + email);
+//        }
+//
+//        if (students.isEmpty()) {
+//            System.out.println("Inserting student ");
+//            repository.insert(student);
+//        } else {
+//            System.out.println("student already exist");
+//        }
+//    }
 
 }
 
--- src/main/java/com/example/mongoTest/StudentRepository.java
+++ src/main/java/com/example/mongoTest/StudentRepository.java
@@ -1,7 +1,13 @@
 package com.example.mongoTest;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.Optional;
 
 public interface StudentRepository extends MongoRepository<Student,String> {
+    Optional<Student> findStudentByEmail(String email);
 
+//    @Query("")
+//    void test();
 }
