336e6c8c17f56ec7f23457ba602a5e30ca12c057
Piotrek <amrozp@gmail.com>
2021-07-29 18:04:50 +0200

    Rearrange structure

--- src/main/java/com/example/mongoTest/AccessingDataMongodbApplication.java
+++ src/main/java/com/example/mongoTest/AccessingDataMongodbApplication.java
@@ -1,7 +1,6 @@
 package com.example.mongoTest;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,8 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AccessingDataMongodbApplication implements CommandLineRunner {
 
-    @Autowired
-    private CustomerRepository repository;
 
     public static void main(String[] args) {
         SpringApplication.run(AccessingDataMongodbApplication.class, args);
@@ -19,30 +16,6 @@ public class AccessingDataMongodbApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
-        repository.deleteAll();
-
-        // save a couple of customers
-        repository.save(new Customer("Alice", "Smith"));
-        repository.save(new Customer("Bob", "Smith"));
-
-        // fetch all customers
-        System.out.println("Customers found with findAll():");
-        System.out.println("-------------------------------");
-        for (Customer customer : repository.findAll()) {
-            System.out.println(customer);
-        }
-        System.out.println();
-
-        // fetch an individual customer
-        System.out.println("Customer found with findByFirstName('Alice'):");
-        System.out.println("--------------------------------");
-        System.out.println(repository.findByFirstName("Alice"));
-
-        System.out.println("Customers found with findByLastName('Smith'):");
-        System.out.println("--------------------------------");
-        for (Customer customer : repository.findByLastName("Smith")) {
-            System.out.println(customer);
-        }
 
     }
 
---
+++ src/main/java/com/example/mongoTest/Gender.java
@@ -0,0 +1,5 @@
+package com.example.mongoTest;
+
+public enum Gender {
+    MALE,FEMALE
+}
